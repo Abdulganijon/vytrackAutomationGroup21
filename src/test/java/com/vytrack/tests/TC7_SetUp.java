e115088c886622d3b2e247dedc372b20c1dfcfdb
Abdulgani <amirzoev088@gmail.com>
2022-10-07 16:47:43 -0500

    Created TC7_SetUp file

---
+++ src/test/java/com/vytrack/tests/TC7_SetUp.java
@@ -0,0 +1,17 @@
+package com.vytrack.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TC7_SetUp {
+    @BeforeMethod
+    public void setUp() {
+
+    }
+
+    @Test
+    public static void test1(){
+
+    }
+
+}
