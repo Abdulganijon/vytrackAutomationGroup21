d0e345e3ed0a30f8444605254f4b376d36e17b87
Alen <almendovic@gmail.com>
2022-10-11 22:22:56 -0600

    Alen created class

--- src/test/java/com/vytrack/tests/Alen_SetUp.java
+++ src/test/java/com/vytrack/tests/Alen_SetUp.java
@@ -5,6 +5,7 @@ import com.vytrack.utilities.logIn;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -31,6 +32,7 @@ public class Alen_SetUp {
         driver.get("https://qa1.vytrack.com/user/login");
         logIn.crm_login(driver, "storemanager75", "UserUser123");
         logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
 
         // Select selectFleet = new Select(driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[2]/a/span")));
         //Thread.sleep(2000);
@@ -45,11 +47,84 @@ public class Alen_SetUp {
         Thread.sleep(3000);
         selectCalendarEvents.click();
 
-        WebElement selectRepeat= driver.findElement(By.xpath("//input[@id='recurrence-repeat-view652']"));
-        Thread.sleep(3000);
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
         selectRepeat.click();
 
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
 
+     /*String actualNumber=checkNumber.getText();
+    String  expectedNumber="1";
+
+     Assert.assertEquals(actualNumber,expectedNumber);*/
+
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
+
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
+
+
+    }
+
+    @Test
+    public void testStoreManager78() throws InterruptedException {
+        driver.get("https://qa1.vytrack.com/user/login");
+        logIn.crm_login(driver, "storemanager78", "UserUser123");
+        logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
+
+        // Select selectFleet = new Select(driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[2]/a/span")));
+        //Thread.sleep(2000);
+        //selectFleet.selectByVisibleText("Vehicles");
+        WebElement selectActivity = driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[5]/a/span"));
+        Thread.sleep(3000);
+        selectActivity.click();
+
+        WebElement selectCalendarEvent = driver.findElement(By.xpath("//span[.='Calendar Events']"));
+        selectCalendarEvent.click();
+        WebElement selectCalendarEvents = driver.findElement(By.xpath("//a[normalize-space()='Create Calendar event']"));
+        Thread.sleep(3000);
+        selectCalendarEvents.click();
+
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
+        selectRepeat.click();
+
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
+
+     /*String actualNumber=checkNumber.getText();
+    String  expectedNumber="1";
+
+     Assert.assertEquals(actualNumber,expectedNumber);*/
+
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
+
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
 
 
     }
