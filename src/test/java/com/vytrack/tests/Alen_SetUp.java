25abee98adbfe6b94eb1c20e78e5e1e895752112
Alen <almendovic@gmail.com>
2022-10-11 23:03:45 -0600

    Alen created class

--- src/test/java/com/vytrack/tests/Alen_SetUp.java
+++ src/test/java/com/vytrack/tests/Alen_SetUp.java
@@ -27,16 +27,17 @@ public class Alen_SetUp {
 
     }
 
-    @Test
+    @Test(
+            priority = 1
+    )
+
+
     public void testStoreManager75() throws InterruptedException {
         driver.get("https://qa1.vytrack.com/user/login");
         logIn.crm_login(driver, "storemanager75", "UserUser123");
         logIn.verifyTitle(driver, "Dashboard");
         Thread.sleep(3000);
 
-        // Select selectFleet = new Select(driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[2]/a/span")));
-        //Thread.sleep(2000);
-        //selectFleet.selectByVisibleText("Vehicles");
         WebElement selectActivity = driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[5]/a/span"));
         Thread.sleep(3000);
         selectActivity.click();
@@ -54,10 +55,6 @@ public class Alen_SetUp {
 
         WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
 
-     /*String actualNumber=checkNumber.getText();
-    String  expectedNumber="1";
-
-     Assert.assertEquals(actualNumber,expectedNumber);*/
 
         String validateNumber = checkNumber.getAttribute("value");
         System.out.println("validateNumber = " + validateNumber);
@@ -78,16 +75,15 @@ public class Alen_SetUp {
 
     }
 
-    @Test
+    @Test(
+            priority = 2
+    )
     public void testStoreManager78() throws InterruptedException {
         driver.get("https://qa1.vytrack.com/user/login");
         logIn.crm_login(driver, "storemanager78", "UserUser123");
         logIn.verifyTitle(driver, "Dashboard");
         Thread.sleep(3000);
 
-        // Select selectFleet = new Select(driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[2]/a/span")));
-        //Thread.sleep(2000);
-        //selectFleet.selectByVisibleText("Vehicles");
         WebElement selectActivity = driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[5]/a/span"));
         Thread.sleep(3000);
         selectActivity.click();
@@ -105,10 +101,185 @@ public class Alen_SetUp {
 
         WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
 
-     /*String actualNumber=checkNumber.getText();
-    String  expectedNumber="1";
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
 
-     Assert.assertEquals(actualNumber,expectedNumber);*/
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
+
+
+    }
+
+    @Test(
+            priority = 3
+    )
+    public void testSalesManager133() throws InterruptedException {
+        driver.get("https://qa1.vytrack.com/user/login");
+        logIn.crm_login(driver, "salesmanager133", "UserUser123");
+        logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
+
+        WebElement selectActivity = driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[5]/a/span"));
+        Thread.sleep(3000);
+        selectActivity.click();
+
+        WebElement selectCalendarEvent = driver.findElement(By.xpath("//span[.='Calendar Events']"));
+        selectCalendarEvent.click();
+        WebElement selectCalendarEvents = driver.findElement(By.xpath("//a[normalize-space()='Create Calendar event']"));
+        Thread.sleep(3000);
+        selectCalendarEvents.click();
+
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
+        selectRepeat.click();
+
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
+
+
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
+
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
+    }
+
+    @Test(
+            priority = 4
+    )
+    public void testSalesManager136() throws InterruptedException {
+        driver.get("https://qa1.vytrack.com/user/login");
+        logIn.crm_login(driver, "salesmanager136", "UserUser123");
+        logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
+
+        WebElement selectActivity = driver.findElement(By.xpath("//div[@id='main-menu']/ul/li[5]/a/span"));
+        Thread.sleep(3000);
+        selectActivity.click();
+
+        WebElement selectCalendarEvent = driver.findElement(By.xpath("//span[.='Calendar Events']"));
+        selectCalendarEvent.click();
+        WebElement selectCalendarEvents = driver.findElement(By.xpath("//a[normalize-space()='Create Calendar event']"));
+        Thread.sleep(3000);
+        selectCalendarEvents.click();
+
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
+        selectRepeat.click();
+
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
+
+
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
+
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
+    }
+
+    @Test(
+            priority = 5
+    )
+    public void testUser44() throws InterruptedException {
+        driver.get("https://qa1.vytrack.com/user/login");
+        logIn.crm_login(driver, "user44", "UserUser123");
+        logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
+
+        WebElement selectActivity = driver.findElement(By.xpath("(//span[@class='title title-level-1'][normalize-space()='Activities'])[1]"));
+        Thread.sleep(3000);
+        selectActivity.click();
+
+        WebElement selectCalendarEvent = driver.findElement(By.xpath("//span[.='Calendar Events']"));
+        selectCalendarEvent.click();
+
+        WebElement selectCalendarEvents = driver.findElement(By.xpath("//a[normalize-space()='Create Calendar event']"));
+        Thread.sleep(3000);
+        selectCalendarEvents.click();
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
+        selectRepeat.click();
+
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
+
+
+        String validateNumber = checkNumber.getAttribute("value");
+        System.out.println("validateNumber = " + validateNumber);
+
+        Thread.sleep(3000);
+        WebElement deteleNumber = driver.findElement(By.xpath("(//input[@value='1'])[2]"));
+        deteleNumber.clear();
+
+
+        WebElement errorMsg = driver.findElement(By.xpath("(//span[.='This value should not be blank.'])[3]"));
+
+        Assert.assertTrue(errorMsg.isDisplayed());
+        System.out.println("errorMsg.getText() = " + errorMsg.getText());
+
+
+        driver.close();
+
+
+    }
+
+    @Test(
+            priority = 6
+    )
+    public void testUser49() throws InterruptedException {
+        driver.get("https://qa1.vytrack.com/user/login");
+        logIn.crm_login(driver, "user49", "UserUser123");
+        logIn.verifyTitle(driver, "Dashboard");
+        Thread.sleep(3000);
+
+        WebElement selectActivity = driver.findElement(By.xpath("(//span[@class='title title-level-1'][normalize-space()='Activities'])[1]"));
+        Thread.sleep(3000);
+        selectActivity.click();
+
+        WebElement selectCalendarEvent = driver.findElement(By.xpath("//span[.='Calendar Events']"));
+        selectCalendarEvent.click();
+
+        WebElement selectCalendarEvents = driver.findElement(By.xpath("//a[normalize-space()='Create Calendar event']"));
+        Thread.sleep(3000);
+        selectCalendarEvents.click();
+
+        WebElement selectRepeat = driver.findElement(By.xpath("//label[normalize-space()='Repeat']"));
+        Thread.sleep(5000);
+        selectRepeat.click();
+
+        WebElement checkNumber = driver.findElement(By.xpath("(//input[@value='1'])[2] "));
+
 
         String validateNumber = checkNumber.getAttribute("value");
         System.out.println("validateNumber = " + validateNumber);
